fedae9f0c9a2e41e5a858a1d1558bfd4981c0a9f
shawn edgell <shawn_edgell@hotmail.com>
2023-10-09 22:45:14 -0400

    fixed dist directory for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
+import { defineConfig } from 'vite';
 import ViteSvg from 'vite-plugin-svgr';
 
-export default {
+export default defineConfig({
   plugins: [ViteSvg()],
-  base: '/form-validation/'
-};
+  base: '/form-validation/',
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  build: {
+    target: 'esnext', 
+    outDir: 'dist', 
+    minify: 'terser',
+    sourcemap: true
+  },
+  server: {
+    port: 3000, 
+    open: true,   
+    strictPort: false, 
+    cors: true,   
+    https: false,  
+    proxy: { 
+    }
+  }
+});
